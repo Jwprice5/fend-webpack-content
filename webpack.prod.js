bd27410ad58481b2a85ca40c3dd895a86e623382
John Price <jwprice5@gmail.com>
2020-04-11 13:52:04 -0600

    Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	modified:   src/client/views/index.html 	modified:   webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,9 +2,16 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
+    optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    },
     mode: 'production',
     devtool: 'source-map',
     watch: true,
@@ -22,7 +29,7 @@ module.exports = {
             // remember that sass can't have quotes!
             {
                 test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
+                use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
             }
         ]
     },
@@ -39,6 +46,8 @@ module.exports = {
             // Automatically remove all unused webpack assets on rebuild
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
-        })
+        }),
+        new MiniCssExtractPlugin({filename: '[name].css'}),
+        new WorkboxPlugin.GenerateSW()
     ]
 }
