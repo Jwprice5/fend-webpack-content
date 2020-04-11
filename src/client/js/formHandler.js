1c60d43f717cb4fe74a82f17f07896ef26271e79
John Price <jwprice5@gmail.com>
2020-04-11 13:10:56 -0600

    Changes to be committed: 	modified:   .babelrc 	modified:   src/client/js/formHandler.js

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -7,10 +7,12 @@ function handleSubmit(event) {
     Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
+ /*   fetch('http://localhost:8080/test') */
+    fetch('http://api.openweathermap.org/data/2.5/weather?zip=80013&appid=a57db8cc0826e3bdf6d50bc381ea8998')
     .then(res => res.json())
     .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+        /*document.getElementById('results').innerHTML = res.message */
+        document.getElementById('results').innerHTML = res.main.temp
     })
 }
 
