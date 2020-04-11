3803ef1040a698cb8b7ac25d8be4bea0e057426f
John Price <jwprice5@gmail.com>
2020-04-11 10:06:41 -0600

    Changes to be committed: 	modified:   src/client/index.js 	modified:   src/client/js/formHandler.js 	renamed:    src/client/styles/base.css -> src/client/styles/base.scss 	renamed:    src/client/styles/footer.css -> src/client/styles/footer.scss 	renamed:    src/client/styles/form.css -> src/client/styles/form.scss 	renamed:    src/client/styles/header.css -> src/client/styles/header.scss 	modified:   src/client/views/index.html 	modified:   src/server/index.js 	modified:   webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -6,12 +6,23 @@ const { CleanWebPackPlugin } = require('clean-webpack-plugin')
 module.exports = {
     entry: './src/client/index.js',
     mode: 'production',
+    devtool: 'source-map',
+    watch: true,
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
     module: {
         rules: [
             {
-                test: '/\.js$/',
+                test: /\.js$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            // remember that sass can't have quotes!
+            {
+                test: /\.sccs$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
             }
         ]
     },
